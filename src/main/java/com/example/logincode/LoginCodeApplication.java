3db8a6c38cdff09547e08d381b1305e7a5da335d
[J-neat] <[junghyo0258@catholic.ac.kr]>
2024-02-19 16:09:36 +0900

    Initial commit

---
+++ src/main/java/com/example/logincode/LoginCodeApplication.java
@@ -0,0 +1,13 @@
+package com.example.logincode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginCodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoginCodeApplication.class, args);
+    }
+
+}
